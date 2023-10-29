b18bd5c3595bb92c9cc8d9480934521bf2a06b40
Jason Davis <jason.davis1025@gmail.com>
2023-10-29 15:57:22 -0700

    new project upload

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css'],
+})
+export class TaskItemComponent {
+  faTimes = faTimes;
+  @Input() task: Task = {
+    id: 0,
+    text: '',
+    day: '',
+    reminder: true,
+  };
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+
+  onDelete(task: any) {
+    console.log(task);
+    this.onDeleteTask.emit(task);
+  }
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
+  }
+}
